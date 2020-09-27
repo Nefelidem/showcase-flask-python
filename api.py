db00d02ae3634b06dd6d996641e17fe23ef35bc4
Nefeli <Nefelidem@users.noreply.github.com>
2020-09-27 16:54:03 +0300

    one file OO

--- api.py
+++ api.py
@@ -6,37 +6,34 @@ import secrets
 from jinja2 import Template
 import jinja2
 
+REDIRECT_URL = 'http://127.0.0.1:5000/results'
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
-class AuthenteqApi:
-
-    def __init__(self):
-        self.REDIRECT_URL = 'http://127.0.0.1:5000/results'
-        self.CLIENT_ID = os.environ.get('CLIENT_ID')
-        self.CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-
-    def verification_result(self):
-        value = a.request_verification()
-        code = request.args.get('code')
-        if code:
-            results_url = ("https://api.app.authenteq.com/web-idv/verification-result?code={}".format(code) +
-                           "&redirectUrl={}".format(REDIRECT_URL))
-            auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-            response = requests.request('GET', results_url, auth=auth)
-            details_response = response.json()
-            if 'errorCode' not in details_response:
-                return render_template("results_table.html", platform=details_response['platform'],
-                                       status=details_response['status'],
-                                       id=details_response['id'],
-                                       starttime=details_response['startTime'],
-                                       doctype=details_response['documentData']['documentType'],
-                                       docnumber=details_response['documentData']['documentNumber'],
-                                       issueCountry=details_response['documentData']['issuingCountry'],
-                                       name=details_response['documentData']['firstName'],
-                                       lastname=details_response['documentData']['lastName'],
-                                       given_name=details_response['documentData']['givenNames'],
-                                       Surname=details_response['documentData']['surname'],
-                                       birthday=details_response['documentData']['dateOfBirth'])
-            elif 'errorCode' in details_response:
-                print('details_response')
-        else:
-            return redirect(value)
+def verification_result(self):
+    print('edo', CLIENT_ID)
+    value = a.request_verification()
+    code = request.args.get('code')
+    if code:
+        results_url = ("https://api.app.authenteq.com/web-idv/verification-result?code={}".format(code) +
+                       "&redirectUrl={}".format(REDIRECT_URL))
+        auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+        response = requests.request('GET', results_url, auth=auth)
+        details_response = response.json()
+        if 'errorCode' not in details_response:
+            return render_template("results_table.html", platform=details_response['platform'],
+                                   status=details_response['status'],
+                                   id=details_response['id'],
+                                   starttime=details_response['startTime'],
+                                   doctype=details_response['documentData']['documentType'],
+                                   docnumber=details_response['documentData']['documentNumber'],
+                                   issueCountry=details_response['documentData']['issuingCountry'],
+                                   name=details_response['documentData']['firstName'],
+                                   lastname=details_response['documentData']['lastName'],
+                                   given_name=details_response['documentData']['givenNames'],
+                                   Surname=details_response['documentData']['surname'],
+                                   birthday=details_response['documentData']['dateOfBirth'])
+        elif 'errorCode' in details_response:
+            print('details_response')
+    else:
+        return redirect(value)
